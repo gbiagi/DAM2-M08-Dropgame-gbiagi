5a416fe9cfa0eff0cfa758647f272ea2f5ef723d
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2024-03-12 19:21:13 +0100

    update

--- core/src/com/gbiagi/game/Dropgame.java
+++ core/src/com/gbiagi/game/Dropgame.java
@@ -2,12 +2,18 @@ package com.gbiagi.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Dropgame extends ApplicationAdapter {
 	private Texture dropImage;
@@ -16,12 +22,29 @@ public class Dropgame extends ApplicationAdapter {
 	private Music rainMusic;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
+	private Rectangle bucket;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
 
 	@Override
 	public void create () {
+		// create the camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		// create a SpriteBatch
+		batch = new SpriteBatch();
+
 		// load the images for the droplet and the bucket, 64x64 pixels each
 		dropImage = new Texture(Gdx.files.internal("droplet.png"));
 		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		bucket = new Rectangle();
+		bucket.x = (float) (800 / 2 - 64 / 2);
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
 
 		// load the drop sound effect and the rain background "music"
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
@@ -29,10 +52,30 @@ public class Dropgame extends ApplicationAdapter {
 
 		// start the playback of the background music immediately
 		rainMusic.setLooping(true);
-		rainMusic.play();	}
+		rainMusic.play();
+	}
 
 	@Override
 	public void render () {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
+
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
 
 	}
 	
@@ -40,4 +83,15 @@ public class Dropgame extends ApplicationAdapter {
 	public void dispose () {
 
 	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+
 }
