1dbd5329b6176d626bfed6839c33fe1d196866e9
geanfrancobiagi <geanfrancobiagi@hotmail.com>
2024-03-18 17:00:39 +0100

    DropGame2 Ok

--- android/src/com/gbiagi/game/AndroidLauncher.java
+++ android/src/com/gbiagi/game/AndroidLauncher.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.gbiagi.game.Dropgame;
 
 public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +11,6 @@ public class AndroidLauncher extends AndroidApplication {
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useAccelerometer = false;
 		config.useCompass = false;
-		initialize(new Dropgame(), config);
+		initialize(new Drop(), config);
 	}
 }
